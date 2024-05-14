4a78769eab959e9a44cb20e4f65e8d05e2a4e95b
Deana Chou <130136996+deanachou@users.noreply.github.com>
2024-05-14 10:30:37 +0900

    db setup

---
+++ server/db/migrations/20240514012449_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id").primary();
+    table.string("username", 32).unique().notNullable();
+    table.string("hashed_password").notNullable();
+    table.string("salt");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
